85b0116bab145e87845f0b7cb8dccea5d198ed90
radhaSaoji <saojiradha@gmail.com>
2021-07-13 08:50:37 +0530

    changes in branchB1

--- src/Package2/Class2.java
+++ src/Package2/Class2.java
@@ -4,7 +4,8 @@ public class Class2 {
 	
 	public static void main(String[] args) {
 		System.out.println("2");
-		System.out.println("2.1");
+		System.out.println("2.1- changed in branch B1");
+		System.out.println("hey, i am in branch B1");
 	}
 
 }
